b4b7d1f6366f14329abdf9d0507e1bcd03a8e586
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2024-01-04 01:20:21 +0300

    ui updates

--- src/app/(account)/_components/aside/navigation.tsx
+++ src/app/(account)/_components/aside/navigation.tsx
@@ -1,12 +1,20 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import usernavigations from "@/mocks/usernavigations";
+import { usePathname } from "next/navigation";
 export default function Navigation() {
+  const pathname = usePathname();
   return (
     <div>
       <ul className="space-y-2">
         {Object.entries(usernavigations).map((entry, index) => (
-          <li key={index} className="hover:text-primary transition-colors">
+          <li
+            key={index}
+            className={`hover:text-primary transition-colors ${
+              pathname === entry[1].href ? "text-primary" : ""
+            }`}
+          >
             <Link href={entry[1].href} className="text-lg py-2 px-2 block">
               {entry[1].title}
             </Link>
--- src/app/(products)/[slug]/layout.tsx
+++ src/app/(products)/[slug]/layout.tsx
@@ -3,18 +3,18 @@ import dynamic from "next/dynamic";
 import { SidebarSkeleton } from "@/components/Aside/Sidebar";
 
 const Sidebar = dynamic(() => import("@/components/Aside/Sidebar"), {
-    ssr: false,
-    loading: SidebarSkeleton,
+  ssr: false,
+  loading: SidebarSkeleton,
 });
 
 export default function layout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <div className="flex container">
-                <Sidebar />
+  return (
+    <>
+      <div className="flex container">
+        <Sidebar />
 
-                <div className="flex-1">{children}</div>
-            </div>
-        </>
-    );
+        <div className="flex-1">{children}</div>
+      </div>
+    </>
+  );
 }
